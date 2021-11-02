b8a7cf99df07b737e4c27452e46a8fae360283ef
abettini99 <abettini99@gmail.com>
2021-11-02 22:07:03 +0100

    Dual grid + E10 matrix
    Added dual grid data to Grid2D , also fixed a bit of indexing (grid_gen.py).
    
    Added E10 matrix and fixed weird E21 bug where last entry isnt actually noted, by creating the sparse matrix with an extra row / column, and then removing that column at the end of the sparse matrix construction.

--- lib/grid_gen.py
+++ lib/grid_gen.py
@@ -65,6 +65,38 @@ class Grid2D:
            and the other two indices refer to the primal cell index in the x-y
            frame.
 
+           --
+
+       get_boundary_dual_faces_idx() -> list[ array_like ] ( [4 -> [x]] ), array_like
+           returns indices of all boundary dual faces in N,E,S,W format in grid
+           form. Also returns the list in a flattened, sorted array.
+
+       get_all_dual_cells_idx() -> array_like ( [N+2, M+2] )
+           returns indices of all dual cells in grid form.
+
+       get_all_dual_faces_idx() -> list[ array_like ] ( [2 -> [x,x]] )
+           returns indices of all dual faces in V, H format and in grid form.
+
+       get_all_dual_verts_idx() -> array_like ( [N+1, M+1] )
+           returns indices of all dual vertices in grid form.
+
+       get_all_dual_cell2face_connectivity() -> array_like ( [4, N+2, M+2] )
+           returns indices of all dual faces connected to each dual cell in
+           grid form. The first index refers to the direction (N E S W), and the
+           other two indices refer to the dual cell index in the x-y frame.
+
+       get_all_dual_vert2face_connectivity() -> array_like ( [4, N+1, M+1] )
+           returns indices of all dual faces connected to each dual vertex in
+           grid form. The first index refers to the direction (N E S W), and
+           the other two indices refer to the dual vertex index in the x-y
+           frame.
+
+       get_internal_dual_cell2vert_connectivity() -> array_like ( [4, N+1, M+1] )
+           returns indices of all dual vertices connected to each interior dual
+           cell in grid form. The first index refers to the direction (SW SE NE
+           NW), and the other two indices refer to the dual cell index in the
+           x-y frame.
+
        Notes
        -----
        An index of -1 refers to a spurious datapoint (e.g. face or cell).
@@ -90,14 +122,23 @@ class Grid2D:
     # b     - boundary
     # mat   - matrix
 
-    matPcell_idx:        npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [N+2, M+2]      // cell numbering, real, boundary, and spurious cells
-    matPface_idx:        list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [2 -> [x,x]]    // face numbering, real, boundary, and spurious faces
-    matPvert_idx:        npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [N+1, M+1]      // vertices numbering, real vertices
-    matPcellface_idx:    npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+2, M+2]   // cell-face connectivity, real, boundary, and spurious faces
-    matPvertface_idx:    npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+1, M+1]   // vertex-face connectivity, real and spurious faces
-    matPcellvert_idx:    npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N, M]       // cell-vertex connectivity, real cells
-    bPcells_idx:         list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [4 -> [x]]      // boundary cell indicess
-    bPface_idx:          list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [4 -> [x]]      // boundary cell indicess
+    matPcell_idx:       npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [N+2, M+2]      // primal cell numbering, real, boundary, and spurious cells
+    matPface_idx:       list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [2 -> [x,x]]    // primal face numbering, real, boundary, and spurious faces
+    matPvert_idx:       npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [N+1, M+1]      // primal vertices numbering, real vertices
+    matPcellface_idx:   npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+2, M+2]   // primal cell-face connectivity, real, boundary, and spurious faces
+    matPvertface_idx:   npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+1, M+1]   // primal vertex-face connectivity, real and spurious faces
+    matPcellvert_idx:   npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N, M]       // primal cell-vertex connectivity, real cells
+    bPcells_idx:        list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [4 -> [x]]      // primal boundary cell indices
+    bPface_idx:         list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [4 -> [x]]      // primal boundary cell indices
+
+    # # TODO: update comments
+    matDcell_idx:       npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [N+1, M+1]      // dual cell numbering, real cells
+    matDface_idx:       list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [2 -> [x,x]]    // dual face numbering, real and spurious faces
+    matDvert_idx:       npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [N+2, M+2]      // dual vertex numbering, real and spurious vertex
+    matDcellface_idx:   npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+2, M+2]   // dual cell-face connectivity, real and spurious faces
+    matDvertface_idx:   npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+1, M+1]   // dual vertex-face connectivity, real and spurious faces
+    matDcellvert_idx:   npt.NDArray[np.int32]       = field(init=False, repr=False)   ## [4, N+1, M+1]   // dual cell-vertex connectivity, real cells, disparity between primal and this is that N+1 and M+1 dual cells exist, not N, M.
+    bDface_idx:         list[npt.NDArray[np.int32]] = field(init=False, repr=False)   ## [4 -> [x]]      // primal boundary cell indices
 
     def __post_init__(self) -> None:
         #### ======================== ####
@@ -109,18 +150,33 @@ class Grid2D:
         matPcellface_idx:   npt.NDArray[np.int32]
         matPvertface_idx:   npt.NDArray[np.int32]
         matPcellvert_idx:   npt.NDArray[np.int32]
-        bPcells_idx:        list[npt.NDArray[np.int32]]
-        bPface_idx:         list[npt.NDArray[np.int32]]
         matPfaceH_idx:      npt.NDArray[np.int32]
         matPfaceV_idx:      npt.NDArray[np.int32]
         matPcellmask_idx:   npt.NDArray[np.bool_]
         matPfacemaskH_idx:  npt.NDArray[np.bool_]
         matPfacemaskV_idx:  npt.NDArray[np.bool_]
+
+        matDcell_idx:       npt.NDArray[np.int32]
+        matDface_idx:       list[npt.NDArray[np.int32]]
+        matDvert_idx:       npt.NDArray[np.int32]
+        matDcellface_idx:   npt.NDArray[np.int32]
+        matDvertface_idx:   npt.NDArray[np.int32]
+        matDcellvert_idx:   npt.NDArray[np.int32]
+        matDfaceH_idx:      npt.NDArray[np.int32]
+        matDfaceV_idx:      npt.NDArray[np.int32]
+        matDcellmask_idx:   npt.NDArray[np.bool_]
+        matDfacemaskH_idx:  npt.NDArray[np.bool_]
+        matDfacemaskV_idx:  npt.NDArray[np.bool_]
+
+        bPcells_idx:        list[npt.NDArray[np.int32]]
+        bPface_idx:         list[npt.NDArray[np.int32]]
+        bDface_idx:         list[npt.NDArray[np.int32]]
         tmp:                npt.NDArray[np.int32] = np.asarray(0, dtype=np.int32)
 
         #### ================================= ####
         #### primal cell-level data generation ####
         #### ================================= ####
+        ## Note that the center of the primal cell is the center.
         matPcell_idx      = np.arange( (self.N+2)*(self.M+2), dtype=np.int32 )                  ## Create full grid index (real, boundary, spurious cells)
         matPcell_idx      = matPcell_idx.reshape( (self.N+2,self.M+2) )                         ## Reshape it into a grid
         matPcell_idx[0,0] = matPcell_idx[-1,0] = matPcell_idx[0,-1] = matPcell_idx[-1,-1] = -1  ## Set spurious index to -1 -- always the corner cases
@@ -159,9 +215,9 @@ class Grid2D:
         matPvert_idx                    = np.arange( (self.N+1)*(self.M+1), dtype=np.int32 )    ## Create full grid index for vertices
         matPvert_idx                    = matPvert_idx.reshape( (self.N+1,self.M+1) )           ## Reshape it into grid
 
-        #### ============================ ####
-        #### connectivity data generation ####
-        #### ============================ ####
+        #### =================================== ####
+        #### primal connectivity data generation ####
+        #### =================================== ####
         ## Generate primal cell-face connectivity data
         matPcellface_idx               = np.empty( (4,(self.N+2),(self.M+2)), dtype=np.int32 )
         matPcellface_idx[dir2D.N,:,:]  = matPface_idx[dir2D.H][1:,:];
@@ -183,28 +239,105 @@ class Grid2D:
         matPcellvert_idx[dir2D.NE,:,:] = matPvert_idx[1: , 1:];
         matPcellvert_idx[dir2D.NW,:,:] = matPvert_idx[1: ,:-1];
 
-        #### ======================= ####
-        #### boundary data generation ####
-        #### ======================= ####
-        ## Generate boundary primal cell data, simple slicing
+        #### ================================= ####
+        #### dual vertex-level data generation ####
+        #### ================================= ####
+        ## The vertices of the dual grid coincide with the center of the primal cells.
+        matDvert_idx = np.copy(matPcell_idx)
+
+        #### =============================== ####
+        #### dual face-level data generation ####
+        #### =============================== ####
+        ## The face of the dual grid cell **nearly** coincide with the **numbering** of the primal faces
+        ## The main difference being that the boundary faces do not exist.
+        ## Additionally, note that the x-derivatives given first, so the horizontal faces come first!
+        matDface_idx  = [tmp, tmp]
+
+        matDfaceH_idx                     = np.arange( (self.N+2)*(self.M+3), dtype=np.int32 )   ## Create full grid index for vertical faces
+        matDfaceH_idx                     = matDfaceH_idx.reshape( (self.N+2,self.M+3) )         ## Reshape it into grid
+        matDfaceH_idx[:,0] = matDfaceH_idx[:,-1]= -1
+        matDfacemaskH_idx = matDfaceH_idx >= 0
+        matDfaceH_idx[matDfacemaskH_idx] -= 1
+        for i in range(1,self.N+2):
+            matDfaceH_idx[i][matDfacemaskH_idx[i]] -= 2*i
+
+        matDfaceV_idx                     = np.arange( (self.N+3)*(self.M+2), dtype=np.int32 )   ## Create full grid index for vertical faces
+        matDfaceV_idx                     = matDfaceV_idx.reshape( (self.N+3,self.M+2) )         ## Reshape it into grid
+        matDfaceV_idx[0,:] = matDfaceV_idx[-1,:] = -1
+        matDfacemaskV_idx = matDfaceV_idx >= 0
+        matDfaceV_idx[matDfacemaskV_idx] -= self.M+1
+        matDfaceV_idx[matDfacemaskV_idx] += matDfaceH_idx.max()
+
+        matDface_idx[dir2D.H] = matDfaceH_idx
+        matDface_idx[dir2D.V] = matDfaceV_idx
+
+        # print(matDfaceH_idx)
+        # print(matDfaceV_idx)
+        # print(matDvert_idx)
+        #### =============================== ####
+        #### dual cell-level data generation ####
+        #### =============================== ####
+        ## The cells of the dual grid coincide with the **numbering** of the primal vertices.
+        matDcell_idx = np.copy(matPvert_idx)
+
+        #### ================================= ####
+        #### dual connectivity data generation ####
+        #### ================================= ####
+        ## Generate dual cell-face connectivity data
+        matDcellface_idx               = np.empty( (4,(self.N+1),(self.M+1)), dtype=np.int32 )
+        matDcellface_idx[dir2D.N,:,:]  = matDface_idx[dir2D.H][1:,1:-1];
+        matDcellface_idx[dir2D.E,:,:]  = matDface_idx[dir2D.V][1:-1,1:];
+        matDcellface_idx[dir2D.S,:,:]  = matDface_idx[dir2D.H][:-1,1:-1];
+        matDcellface_idx[dir2D.W,:,:]  = matDface_idx[dir2D.V][1:-1,:-1];
+
+        ## Generate dual vertex-face connectivity data
+        matDvertface_idx               = np.empty( (4,(self.N+2),(self.M+2)), dtype=np.int32 )
+        matDvertface_idx[dir2D.N,:,:]  = matDface_idx[dir2D.V][1: ,:];
+        matDvertface_idx[dir2D.E,:,:]  = matDface_idx[dir2D.H][:  ,1:];
+        matDvertface_idx[dir2D.S,:,:]  = matDface_idx[dir2D.V][:-1,:];
+        matDvertface_idx[dir2D.W,:,:]  = matDface_idx[dir2D.H][:  ,:-1];
+
+        ## Generate dual cell-vertex connectivity data
+        matDcellvert_idx               = np.empty( (4,(self.N+1),(self.M+1)), dtype=np.int32 )
+        matDcellvert_idx[dir2D.SW,:,:] = matDvert_idx[:-1,:-1];
+        matDcellvert_idx[dir2D.SE,:,:] = matDvert_idx[1: ,:-1];
+        matDcellvert_idx[dir2D.NE,:,:] = matDvert_idx[1: , 1:];
+        matDcellvert_idx[dir2D.NW,:,:] = matDvert_idx[1: ,:-1];
+
+        #### =============================== ####
+        #### primal boundary data generation ####
+        #### =============================== ####
+        ## Generate primal boundary cell data, simple slicing
         bPcells_idx = [tmp, tmp, tmp, tmp]
         bPcells_idx[dir2D.N] = matPcell_idx[-1,:][matPcellmask_idx[-1,:]]
         bPcells_idx[dir2D.E] = matPcell_idx[:,-1][matPcellmask_idx[:,-1]]
         bPcells_idx[dir2D.S] = matPcell_idx[0,:][matPcellmask_idx[0,:]]
         bPcells_idx[dir2D.W] = matPcell_idx[:,0][matPcellmask_idx[:,0]]
 
-        ## Generate boundary face data, simple slicing
+        ## Generate primal boundary face data, simple slicing
         bPface_idx = [tmp, tmp, tmp, tmp]
         bPface_idx[dir2D.N] = matPface_idx[dir2D.H][-1,:][matPfacemaskH_idx[-1,:]]
         bPface_idx[dir2D.E] = matPface_idx[dir2D.V][:,-1][matPfacemaskV_idx[:,-1]]
         bPface_idx[dir2D.S] = matPface_idx[dir2D.H][0,:][matPfacemaskH_idx[0,:]]
         bPface_idx[dir2D.W] = matPface_idx[dir2D.V][:,0][matPfacemaskV_idx[:,0]]
 
+        #### ============================= ####
+        #### dual boundary data generation ####
+        #### ============================= ####
+        ## Generate dual boundary face data, simple slicing
+        bDface_idx = [tmp, tmp, tmp, tmp]
+        bDface_idx[dir2D.N] = matDface_idx[dir2D.H][-1,:][matDfacemaskH_idx[-1,:]]
+        bDface_idx[dir2D.E] = matDface_idx[dir2D.V][:,-1][matDfacemaskV_idx[:,-1]]
+        bDface_idx[dir2D.S] = matDface_idx[dir2D.H][0,:][matDfacemaskH_idx[0,:]]
+        bDface_idx[dir2D.W] = matDface_idx[dir2D.V][:,0][matDfacemaskV_idx[:,0]]
+
         #### ============================ ####
         #### finalize post-initialization ####
         #### ============================ ####
         self.bPcells_idx        = bPcells_idx
         self.bPface_idx         = bPface_idx
+        self.bDface_idx         = bDface_idx
+
         self.matPcell_idx       = matPcell_idx
         self.matPface_idx       = matPface_idx
         self.matPvert_idx       = matPvert_idx
@@ -212,14 +345,21 @@ class Grid2D:
         self.matPvertface_idx   = matPvertface_idx
         self.matPcellvert_idx   = matPcellvert_idx
 
+        self.matDcell_idx       = matDcell_idx
+        self.matDface_idx       = matDface_idx
+        self.matDvert_idx       = matDvert_idx
+        self.matDcellface_idx   = matDcellface_idx
+        self.matDvertface_idx   = matDvertface_idx
+        self.matDcellvert_idx   = matDcellvert_idx
+
     def __str__(self) -> str:
         return  f'===========================\n' + \
                 f'2D Structured Grid (NxM):\nN = {self.N}\nM = {self.M}\n' + \
                 f'===========================\n'
 
     def get_internal_primal_cells_idx(self) -> npt.NDArray[np.int32]:
-        cells : npt.NDArray[np.int32] = self.matPcell_idx[1:-1,1:-1]
-        return cells
+        Pcells: npt.NDArray[np.int32] = self.matPcell_idx[1:-1,1:-1]
+        return Pcells
 
     def get_boundary_primal_cells_idx(self) -> tuple[list[npt.NDArray[np.int32]], npt.NDArray[np.int32]]:
 
@@ -259,8 +399,34 @@ class Grid2D:
         Pconnectivity : npt.NDArray[np.int32] = self.matPcellface_idx[:, 1:-1, 1:-1]
         return Pconnectivity
 
+    def get_boundary_dual_faces_idx(self) -> tuple[list[npt.NDArray[np.int32]], npt.NDArray[np.int32]]:
+
+        flattened_idx: npt.NDArray[np.int32] = self.bDface_idx[0][:]
+        for idx in range(1,len(self.bDface_idx)):
+            flattened_idx = np.hstack( (flattened_idx, self.bDface_idx[idx][:]) )
+
+        return self.bDface_idx, np.sort(flattened_idx)
+
+    def get_all_dual_cells_idx(self) -> npt.NDArray[np.int32]:
+        return self.matDcell_idx
+
+    def get_all_dual_faces_idx(self) -> list[npt.NDArray[np.int32]]:
+        return self.matDface_idx
+
+    def get_all_dual_verts_idx(self) -> npt.NDArray[np.int32]:
+        return self.matDvert_idx
+
+    def get_all_dual_cell2face_connectivity(self) -> npt.NDArray[np.int32]:
+        return self.matDcellface_idx
+
+    def get_all_dual_vert2face_connectivity(self) -> npt.NDArray[np.int32]:
+        return self.matDvertface_idx
+
+    def get_internal_dual_cell2vert_connectivity(self) -> npt.NDArray[np.int32]:
+        return self.matDcellvert_idx
+
 if __name__ == "__main__":
-    N = 6
+    N = 3
     M = 3
     grid = Grid2D(N,M)
     #print(grid.get_boundary_primal_faces_idx())
--- lib/incidence_mat.py
+++ lib/incidence_mat.py
@@ -45,14 +45,12 @@ def construct_E21_2D(grid: Grid2D) -> tuple[sparse.csr_matrix, sparse.csr_matrix
     #### ================== ####
     #### function variables ####
     #### ================== ####
-    N: int                                    = grid.N
-    M: int                                    = grid.M
     matPcell_idx:       npt.NDArray[np.int32] = grid.get_all_primal_cells_idx()
     Pconnectivity_idx:  npt.NDArray[np.int32] = grid.get_all_primal_cell2face_connectivity()
-    vPface_idx:         npt.NDArray[np.int32] = grid.get_boundary_primal_faces_idx()[1]
+    bPface_idx:         npt.NDArray[np.int32] = grid.get_boundary_primal_faces_idx()[1]
 
     # TODO: Convert to COO matrix
-    E21 = sparse.lil_matrix((matPcell_idx.max()+1, Pconnectivity_idx.max()+1),dtype=np.int8)
+    E21 = sparse.lil_matrix((matPcell_idx.max()+1, Pconnectivity_idx.max()+2),dtype=np.int8)
 
     #### ================ ####
     #### E21 construction ####
@@ -62,35 +60,103 @@ def construct_E21_2D(grid: Grid2D) -> tuple[sparse.csr_matrix, sparse.csr_matrix
         for j in range(matPcell_idx.shape[1]): # j index := y index
             idx = matPcell_idx[i,j]
             if idx >= 0: # Avoid doing operations in case of spurious cells (idx < 0)
-                Nidx = Pconnectivity_idx[dir2D.N][i,j]
-                Eidx = Pconnectivity_idx[dir2D.E][i,j]
-                Sidx = Pconnectivity_idx[dir2D.S][i,j]
-                Widx = Pconnectivity_idx[dir2D.W][i,j]
-                E21[idx,Nidx] = 1 if Nidx >= 0 else 0 # Avoid doing operations in case of spurious cells (Nidx, Eidx, ... < 0)
+                Nidx: int = Pconnectivity_idx[dir2D.N][i,j]
+                Eidx: int = Pconnectivity_idx[dir2D.E][i,j]
+                Sidx: int = Pconnectivity_idx[dir2D.S][i,j]
+                Widx: int = Pconnectivity_idx[dir2D.W][i,j]
+                E21[idx,Nidx] = 1 if Nidx >= 0 else 0 # Avoid doing operations in case of spurious faces (Nidx, Eidx, ... < 0)
                 E21[idx,Eidx] = 1 if Eidx >= 0 else 0
                 E21[idx,Sidx] = -1 if Sidx >= 0 else 0
                 E21[idx,Widx] = -1 if Widx >= 0 else 0
+    E21 = E21[:,:-1] # I have no clue why I need to do this, but for some reason this fixes issues with the last row, last column entry.
+    # TODO: fix the E21 bug.
 
     #### ================ ####
     #### E21 finalization ####
     #### ================ ####
-    E21 = E21.tocsr() ## lil_matrix good when constructing, but csr better for matrix-vector products
-    E21.eliminate_zeros() ## In the case that zeros within the sparse structure exists, then remove them
+    E21 = E21.tocsr() # lil_matrix good when constructing, but csr better for matrix-vector products
+    E21.eliminate_zeros() # In the case that zeros within the sparse structure exists, then remove them
 
-    tE21 = remove_sparse_rowcol(E21, cols_idx=vPface_idx)
-    pE21 = extract_sparse_rowcol(E21, idx=vPface_idx, ext='col')
+    tE21 = remove_sparse_rowcol(E21, cols_idx=bPface_idx)
+    pE21 = extract_sparse_rowcol(E21, idx=bPface_idx, ext='col')
     return E21, tE21, pE21
+
+def construct_E10_2D(grid: Grid2D) -> sparse.csr_matrix:
+    """Generates the E10 matrix, used for discrete gradient estimations.
+       Gradient estimation is done on the dual grid, with sink-like vertices,
+       edge orientation left to right, bottom to top.
+
+       Parameters
+       ----------
+       grid : Grid2D
+            Generated structured 2D grid.
+
+       Returns
+       -------
+       E10 : sparse.csr_matrix
+            Sparse E10 matrix (discrete gradient, sink-like vertices, edge
+            orientation left to right, bottom to top)
+
+       Examples
+       --------
+       grid = Grid2D(64,32)
+       E10  = construct_E10_2D(grid)
+       """
+
+    #### ================== ####
+    #### function variables ####
+    #### ================== ####
+    matDvert_idx:       npt.NDArray[np.int32] = grid.get_all_dual_verts_idx()
+    Dconnectivity_idx:  npt.NDArray[np.int32] = grid.get_all_dual_vert2face_connectivity()
+    bDface_idx:         npt.NDArray[np.int32] = grid.get_boundary_dual_faces_idx()[1]
+
+    # TODO: Convert to COO matrix, note that E10 is initialized with an extra row, see comment below.
+    E10 = sparse.lil_matrix((Dconnectivity_idx.max()+2,matDvert_idx.max()+1),dtype=np.int8)
+
+    #### ================ ####
+    #### E10 construction ####
+    #### ================ ####
+    ## Loop through the 2D grid
+    for i in range(matDvert_idx.shape[0]): # i index := x index
+        for j in range(matDvert_idx.shape[1]): # j index := y index
+            idx = matDvert_idx[i,j]
+            if idx >= 0: # Avoid doing operations in case of spurious vertex (idx < 0)
+                Nidx: int = Dconnectivity_idx[dir2D.N][i,j]
+                Eidx: int = Dconnectivity_idx[dir2D.E][i,j]
+                Sidx: int = Dconnectivity_idx[dir2D.S][i,j]
+                Widx: int = Dconnectivity_idx[dir2D.W][i,j]
+                E10[Nidx,idx] = -1 if Nidx >= 0 else 0 # Avoid doing operations in case of spurious faces (Nidx, Eidx, ... < 0)
+                E10[Eidx,idx] = -1 if Eidx >= 0 else 0
+                E10[Sidx,idx] = 1 if Sidx >= 0 else 0
+                E10[Widx,idx] = 1 if Widx >= 0 else 0
+    E10 = E10[:-1,:] # I have no clue why I need to do this, but for some reason this fixes issues with the last row, last column entry.
+    # TODO: fix the E10 bug.
+
+    #### ================ ####
+    #### E10 finalization ####
+    #### ================ ####
+    E10 = E10.tocsr() # lil_matrix good when constructing, but csr better for matrix-vector products
+    E10.eliminate_zeros() # In the case that zeros within the sparse structure exists, then remove them
+    tE10 = remove_sparse_rowcol(E10, rows_idx=bDface_idx)
+    #print(Dconnectivity_idx[dir2D.S])
+    return tE10
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    N = 256
-    M = 256
+    N = 3
+    M = 3
     grid = Grid2D(N,M)
     E21, tE21, pE21 = construct_E21_2D(grid)
+    E10             = construct_E10_2D(grid)
 
-    #print(E21)
-    plt.imshow(pE21.toarray())
+    print(tE21 != -E10.transpose())
+
+    # plt.imshow(E10.toarray()) # # TODO: Maybe something wrong here? Need to troubleshoot
+    # plt.show()
+    plt.imshow(-E10.transpose().toarray())
+    plt.show()
+    plt.imshow(tE21.toarray())
     plt.show()
 
 
